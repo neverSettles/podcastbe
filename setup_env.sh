c8a3fdb56bf6258b45b34003702a94e7cdad8779
neverSettles <christopherRSettles@gmail.com>
2023-09-04 21:32:26 -0700

    Change to apt

--- setup_env.sh
+++ setup_env.sh
@@ -10,8 +10,8 @@ source podenv/bin/activate
 if [ -f "requirements.txt" ]; then
     echo "requirements.txt found. Installing requirements."
     pip3 install -r requirements.txt
-    sudo yum install epel-release
-    sudo yum install ffmpeg ffmpeg-devel
+    sudo apt update
+    sudo apt install ffmpeg
 else
     echo "No requirements.txt file found."
 fi
