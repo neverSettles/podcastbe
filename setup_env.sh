a1ebd478ef70d12ec31b2298334a3aafbf5f3988
neverSettles <christopherRSettles@gmail.com>
2023-09-04 21:30:08 -0700

    Add ffmpeg into setup env instructions

--- setup_env.sh
+++ setup_env.sh
@@ -10,6 +10,8 @@ source podenv/bin/activate
 if [ -f "requirements.txt" ]; then
     echo "requirements.txt found. Installing requirements."
     pip3 install -r requirements.txt
+    sudo yum install epel-release
+    sudo yum install ffmpeg ffmpeg-devel
 else
     echo "No requirements.txt file found."
 fi
