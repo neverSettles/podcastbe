06f7b3f3d4d0327d309bbe70b5122aa12af9ecd9
neverSettles <christopherRSettles@gmail.com>
2023-09-16 18:57:00 -0700

    Change eleven code

--- other/elevenlabs_voices.py
+++ other/elevenlabs_voices.py
@@ -11,9 +11,11 @@ elevenlabs_api_key = os.getenv("ELEVENLABS_API_KEY")
 url = "https://api.elevenlabs.io/v1/voices"
 
 headers = {
-  "Accept": "application/json",
-  "xi-api-key": elevenlabs_api_key
+      "Accept": "audio/mpeg",
+      "Content-Type": "application/json",
+      "xi-api-key": elevenlabs_api_key
 }
+
 response = requests.get(url, headers=headers)
 response = json.loads(response.text)
 
@@ -51,7 +53,27 @@ def convert_to_speech_eleven(text, voice_id):
     
     return response.iter_content(chunk_size=CHUNK_SIZE)
 
-story = "Mat found a glowing stone. At night, it whispered tales of ancient heroes. He became one."
+story = """
+Once upon a time, in a small, sleepy town nestled in the heart of the countryside, there lived a curious and adventurous young boy named Mat. Mat had always been different from the other children in town. While they were content playing in their backyards or cycling around the neighborhood, Mat's heart yearned for something more. He often spent his afternoons gazing out at the horizon, dreaming of far-off lands and thrilling adventures.
+
+Mat's parents, John and Susan, encouraged their son's curiosity and adventurous spirit. They believed that every child should have the opportunity to explore the world around them. John, an avid traveler himself, would often regale Mat with tales of his own adventures in distant lands. Susan, a naturalist, would take Mat on long hikes through the lush forests that surrounded their town, teaching him about the wonders of the natural world.
+
+One bright summer morning, Mat woke up with an overwhelming feeling of restlessness. He couldn't shake the desire to embark on an adventure of his own. He rushed downstairs, where his parents were sipping their morning coffee.
+
+"Mom, Dad, I've decided I want to go on a grand adventure," Mat declared with a determined look in his eyes.
+
+John and Susan exchanged knowing glances. They had expected this day to come sooner or later.
+
+"Mat, that's a wonderful idea," Susan said, smiling warmly. "But remember, with adventure comes responsibility and preparation. You must be ready for whatever challenges may come your way."
+
+John nodded in agreement. "We'll help you plan and prepare for your journey, son. It's important to be well-equipped and informed."
+
+Over the following weeks, Mat immersed himself in books about survival, navigation, and the history of explorers who had charted unknown territories. He also spent countless hours with his father, learning valuable skills like setting up a campfire, reading maps, and using a compass. Mat's mother, Susan, taught him about the plants and animals he might encounter in the wild, emphasizing the importance of respecting and preserving the environment.
+
+Finally, the day of Mat's departure arrived. With a backpack filled with supplies, a map in hand, and the unwavering support of his parents, Mat set off on his grand adventure. His destination was a remote forest on the outskirts of town, a place few had ever ventured into.
+"""
+
+voice_id = "B6iuPRFYgnWhC9SAo8BS" # Tamika
 bytes = convert_to_speech_eleven(story, voice_id)
 
 with open('output/v2/speech.mp3', 'wb') as f:
